04411166a936c689234ad5036f4339d20a3e99fd
Sorokovsky <Sorokovskys@ukr.net>
2026-08-15 12:09:56 +0300

    refactor: backend url

--- src/shared/config/environment.development.ts
+++ src/shared/config/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://nestjs-backend-r2lp.onrender.com',
 } as const;
--- src/shared/config/environment.ts
+++ src/shared/config/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://nestjs-backend-r2lp.onrender.com',
 } as const;
