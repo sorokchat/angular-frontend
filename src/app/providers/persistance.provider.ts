3004f88f53f312a2a93251964393a517263d8014
Sorokovsky <Sorokovskys@ukr.net>
2026-08-18 17:18:06 +0300

    feature: set persistance

--- src/app/providers/persistance.provider.ts
+++ src/app/providers/persistance.provider.ts
@@ -1,9 +1,9 @@
-import { PERSISTANCE_SERVICE, LocalStorageService } from '@/shared';
+import { PERSISTANCE_SERVICE, AngularStorageService } from '@/shared';
 import { type Provider } from '@angular/core';
 
 export function providePersistance(): Provider {
   return {
     provide: PERSISTANCE_SERVICE,
-    useClass: LocalStorageService,
+    useClass: AngularStorageService,
   };
 }
